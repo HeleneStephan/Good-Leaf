9cb04cbc8a1b0d15fe694c605f1be23a59a0ed4c
Helene Stephan <helene.stephan@protonmail.com>
2020-11-04 13:02:13 +0100

    trying to get data from trefle API

--- scripts/random.js
+++ scripts/random.js
@@ -1,1 +1,23 @@
 "use strict";
+
+console.log("test");
+const api = "https://trefle.io/api/v1/";
+const token = "3wvKP6EA2xFo7jZhKNyxnGg2MxOr7sXI-46HKkcTDbw";
+console.log("test2");
+
+async function gettingData() {
+  console.log("test3");
+  //const ediblePlant = document.querySelector("#randomEdiblePlant");
+  try {
+    const response = await fetch(`${api}plants?token=${token}`);
+    console.log("response", response);
+    const jsonData = await response.json();
+    console.log(jsonData);
+  } catch (error) {
+    console.log("error");
+  }
+}
+
+console.log("test4");
+
+gettingData();
